9a2fd45bf5998ea62341e908beddd48e5bc44af3
PHENIXCRIME <photboatxeemus@icloud.com>
2020-09-03 22:13:52 +0700

    create model subject

---
+++ app/Models/Subject.js
@@ -0,0 +1,30 @@
+'use strict'
+
+/** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
+const Model = use('Model')
+
+class Subject extends Model {
+    
+    static get primaryKey() { 
+        return 'subject_id'
+    }
+
+
+    static get createAtColumn(){
+        return null ; 
+    }
+
+    static get updateAtColumn(){
+        return null ; 
+    }
+
+    teacher() { 
+        return this.belongsTo('App/Models/Teacher')
+    }
+
+    enrollment() {
+        return this.hasMany('App/Models/Enrollment')
+    }
+}
+
+module.exports = Subject
