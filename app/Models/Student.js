6cfd887363007bde512ec3ddd9aa4f62a63407a4
PHENIXCRIME <photboatxeemus@icloud.com>
2020-09-03 22:13:31 +0700

    create model student

---
+++ app/Models/Student.js
@@ -0,0 +1,31 @@
+'use strict'
+
+/** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
+const Model = use('Model')
+
+class Student extends Model {
+
+static get primaryKey() { 
+    return 'student_id'
+}
+
+
+static get createAtColumn(){
+    return null ; 
+}
+
+static get updateAtColumn(){
+    return null ; 
+}
+
+group() { 
+    return this.belongsTo('App/Models/Group')
+}
+ 
+enrollment( ){ 
+    return this.hasMany('App/Models/Enrollment')
+}
+
+}
+
+module.exports = Student
