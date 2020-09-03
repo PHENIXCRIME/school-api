106ab1da20543709ff2890841efaf42309eb41b3
PHENIXCRIME <photboatxeemus@icloud.com>
2020-09-03 22:20:19 +0700

    edit EnrollmentController

--- app/Controllers/Http/EnrollmentController.js
+++ app/Controllers/Http/EnrollmentController.js
@@ -1,6 +1,9 @@
 'use strict'
 
 const Database = use('Database')
+const Validator = use
+const enrollment = use('App/Models/Enrollment')
+
 
 function numberTypeParamValidater(number) {
     if (Number.isNaN(parseInt(number) )) 
@@ -13,52 +16,59 @@ function numberTypeParamValidater(number) {
 
 class EnrollmentController {
     async index() { 
-        const enrollments = await Database.table('enrollments')
-
-        return {status:200 ,error:undefined,data:enrollments} 
-
+        const {references = undefined} = request.qs
+        const enrollments = Enrollment.query()
+        if(references) {
+            const extractedReferences =references.split(",")
+            enrollments.with(extractedReferences)
+        }
+        return {status:200 ,error:undefined,data:teachers} 
     }
 
     async show({request}){
         const{id} = request.params
+        const enrollment = await enrollment.find(id) 
+        if (validatedValue.error)
+        return { status: 500, error: validatedValue.error, data: undefined };
 
-        const validatedValue =  numberTypeParamValidater(id)
-        
-        if (validatedValue.error) return {status : 500 ,error: validatedValue.error , data : undefined} 
-        const enrollments = await Database
-        .select('*')
-        .from('enrollments')
-        .where("enrollment_id" , id)
-        .first()
-
-        return{ status :200 ,error:undefined,  data: enrollments || {} }
-
-        // 0 ,"" , false , undefined , null 
-        // return teacher || 
-
+        return{ status :200 ,error:undefined,  data: enrollment || {} }
     }      
 
     async store({request}){
         const { mark , mark_date ,student_id , subject_id} = request.body
+        
+        const enrollment = await Enrollment.find(id)
+        return {status : 200 , error: undefined  , data : enrollment }
+    }
 
-        const missingKeys = []
-
-        if(!mark) missingKey.push('mark')
-        if(!mark_date) missingKey.push('mark_date')
-        if(!student_id) missingKey.push('student_id')
-        if(!subject_id) missingKey.push('subject_id')
-
-
-        if(missingKey.lenght)
-        return { status : 422 , error : `{missingKeys} is missing ` ,data : undefined}
-
-        const enrollments = 
-        await Database
+    async update ({ request }) {
+        const{body ,params} = request 
+        const { id } = params
+        const { mark , mark_date ,student_id , subject_id} = body  
+        const enrollments = await Database
         .table('enrollments')
-        .insert({mark,mark_date,student_id,subject_id })
+        .where({ enrollment_id : id})
+        .update({ mark , mark_date ,student_id , subject_id })
 
-        return {status : 200 , error: undefined  , data : {mark, mark_date ,student_id , subject_id } }
-}
+        const enrollments = await Database
+        .table('enrollments')
+        .where({enrollment_id : enrollment_id}) 
+        .first()
+
+        return {status :200 , error : undefined , data :teachers }   
+    }
+
+    async destroy({request}) {
+        const {id} =request.params
+
+         await Database
+        .table('enrollments')
+        .where({enrollment_id : id})
+        .delete() 
+        
+        return {status : 200 , error : undefined , data : {message : 'success'}}
+    }
+
 }
 
 module.exports = EnrollmentController
