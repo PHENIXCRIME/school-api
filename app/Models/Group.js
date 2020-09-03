34b8b6df6a7de10fc2fd37b9f900276e2f0d5959
PHENIXCRIME <photboatxeemus@icloud.com>
2020-09-03 22:12:54 +0700

    create model group

---
+++ app/Models/Group.js
@@ -0,0 +1,26 @@
+'use strict'
+
+/** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
+const Model = use('Model')
+
+class Group extends Model {
+
+    static get primaryKey() { 
+        return 'group_id'
+    }
+
+
+    static get createAtColumn(){
+        return null ; 
+    }
+
+    static get updateAtColumn(){
+        return null ; 
+    }
+
+    student() { 
+        return this.hasMany('App/Models/Student')
+    }
+}
+
+module.exports = Group
