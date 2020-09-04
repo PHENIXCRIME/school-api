521be51dcb0bf89a4e5f77df617d4232079744fe
PHENIXCRIME <photboatxeemus@icloud.com>
2020-09-04 13:28:02 +0700

    Add boot

--- app/Models/Teacher.js
+++ app/Models/Teacher.js
@@ -2,8 +2,21 @@
 
 /** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
 const Model = use('Model')
+const Hash = use('Hash')
 
 class Teacher extends Model {
+    
+
+    static boot () { 
+        super.boot()
+
+        this.addHook('beforeSave' , async(teacherInstance) => { 
+            if (teacherInstance.dirty.passoword) { 
+                teacherInstance.passoword = await Hash.mmake(teacherInstance.dirty.passoword)
+            }
+        })
+    }
+
     static get primaryKey() { 
         return 'teacher_id'
     }
